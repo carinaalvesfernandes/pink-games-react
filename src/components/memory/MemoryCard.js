217c7e9629f3cd11b46104987f0d6df2ffecd776
carinaalvesfernandes <carinaalvesfernandes@hotmail.com>
2020-09-27 17:00:02 +0200

    Added rotating card animation

--- src/components/memory/MemoryCard.js
+++ src/components/memory/MemoryCard.js
@@ -1,12 +1,23 @@
 import React from "react";
 import "./MemoryCard.css";
 
-function MemoryCard({ image, isFlipped, onClick }) {
+/*function MemoryCard({ image, isFlipped, onClick }) {
   let className = "memory-card";
   if (isFlipped) {
     className = className + " " + image + " image";
   }
   return <div className={className} onClick={onClick}></div>;
+}*/
+
+function MemoryCard({ isFlipped, image, onClick }) {
+  return (
+    <div className="memory-card-scene" onClick={onClick}>
+      <div className={"memory-card-container " + (isFlipped ? "flipped" : "")}>
+        <div className="memory-card back"></div>
+        <div className={`memory-card front image ${image}`}></div>
+      </div>
+    </div>
+  );
 }
 
 export default MemoryCard;
